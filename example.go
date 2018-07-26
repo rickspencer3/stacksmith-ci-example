fc22e25950a5e4c47552d6e10b7a2fe02f25bbac
Marko Mikulicic <mkm@bitnami.com>
2018-07-26 17:09:00 +0200

    Some code

---
+++ example.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+var (
+	listenAddr = flag.String("l", ":80", "listen address and port number")
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello world\n")
+}
+
+func main() {
+	flag.Parse()
+
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(*listenAddr, nil))
+}
